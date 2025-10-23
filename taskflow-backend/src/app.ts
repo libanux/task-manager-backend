a00ab3d025dec4cb5d131ca4342a05235d8d4ae0
unknown <marwamarwazehri@gmail.com>
2025-10-23 16:40:47 +0300

    change origin to true

--- taskflow-backend/src/app.ts
+++ taskflow-backend/src/app.ts
@@ -23,12 +23,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:4200',
-    'https://taskflow-app-2025-odu2kvdtb-marwa-s-projects.vercel.app',
-    'https://taskflow-app-2025.vercel.app',
-    'https://*.vercel.app'  // Allow all Vercel subdomains
-  ],
+  origin:true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With', 'Origin']
